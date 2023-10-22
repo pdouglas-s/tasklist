ad68df5d95a4726c97bc8a8527f40f3cb96bdd1a
Patrick Douglas <pdouglas.s@hotmail.com>
2023-10-21 21:52:46 -0300

    Conclusão API taskList

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionControler';
+import TaskController from './app/controllers/TaskController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionControler.store);
+
+// todas rotas abaixo desse Middleware precisa de autenticação
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+routes.post('/tasks', TaskController.store);
+routes.get('/tasks', TaskController.index);
+routes.put('/tasks/:task_id', TaskController.update);
+routes.delete('/tasks/:task_id', TaskController.delete);
 
 export default routes;
